1af0eeaeaa8053e04752e3e762609da12d0c08c3
MaGaNeT <yhcmaganet@gmail.com>
2022-06-10 01:30:12 +0800

    Change main window's style

--- Entry/MyMain.cpp
+++ Entry/MyMain.cpp
@@ -8,44 +8,42 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ INT nCmdShow)
 {
 	const CHAR* CLASS_NAME = "EngineClass_D3D11_0";
-	const CHAR* WINDOW_NAME	= "Engine_D3D11_0";
-
-	WNDCLASSEX wcex =
+	const CHAR* WINDOW_NAME = "Engine_D3D11_0";
+	HWND windowHandle; WNDCLASSEX wcex = {};
 	{
-		sizeof(WNDCLASSEX),
-		CS_CLASSDC,
-		WndProc,
-		0,
-		0,
-		hInstance,
-		NULL,
-		LoadCursor(NULL, IDC_ARROW),
-		(HBRUSH)(COLOR_WINDOW + 1),
-		NULL,
-		CLASS_NAME,
-		NULL
-	};
-
-	RegisterClassEx(&wcex);
-
-	HWND windowHandle = CreateWindowEx(0,
-		CLASS_NAME,
-		WINDOW_NAME,
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		(ENGINE_SCREEN_WIDTH + GetSystemMetrics(SM_CXDLGFRAME) * 2),
-		(ENGINE_SCREEN_HEIGHT + GetSystemMetrics(SM_CXDLGFRAME) * 2 + GetSystemMetrics(SM_CYCAPTION)),
-		NULL,
-		NULL,
-		hInstance,
-		NULL);
+		wcex.cbSize = sizeof(WNDCLASSEX);
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc = WndProc;
+		wcex.cbClsExtra = 0;
+		wcex.cbWndExtra = 0;
+		wcex.hInstance = hInstance;
+		wcex.hIcon = NULL;
+		wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wcex.hbrBackground = (HBRUSH)(COLOR_BACKGROUND + 1);
+		wcex.lpszMenuName = NULL;
+		wcex.lpszClassName = CLASS_NAME;
+		wcex.hIconSm = NULL;
+		if (!RegisterClassEx(&wcex))
+			return TRUE;
+		RECT clientRect = { 0,0,ENGINE_SCREEN_WIDTH,ENGINE_SCREEN_HEIGHT };
+		::AdjustWindowRect(&clientRect, WS_OVERLAPPEDWINDOW ^ (WS_MAXIMIZEBOX | WS_THICKFRAME), FALSE);
+		windowHandle = CreateWindowEx(
+			0,
+			CLASS_NAME,
+			WINDOW_NAME,
+			(WS_OVERLAPPEDWINDOW ^ (WS_MAXIMIZEBOX | WS_THICKFRAME)),
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			clientRect.right - clientRect.left,
+			clientRect.bottom - clientRect.top,
+			NULL,
+			NULL,
+			hInstance,
+			NULL);
+	}
 
 	CManager::Initialize(windowHandle);
 	CManager* gameManager = CManager::GetManager();
-
-	ShowWindow(windowHandle, nCmdShow);
-	UpdateWindow(windowHandle);
 
 	gameManager->Init();
 
@@ -53,6 +51,9 @@ INT APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	fixedStepTime = static_cast <DOUBLE>(1) / static_cast<DOUBLE>(ENGINE_FIXED_UPDATE_FRAME);
 	updateStepTime = static_cast <DOUBLE>(1) / static_cast<DOUBLE>(ENGINE_UPDATE_FRAME);
 	currentTime = fixedLastTime = updateLastTime = gameManager->GetWindowTimer().GetClockTime();
+
+	::ShowWindow(windowHandle, nCmdShow);
+	::UpdateWindow(windowHandle);
 
 	MSG msg;
 	while (TRUE)
@@ -65,7 +66,7 @@ INT APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 			}
 			else
 			{
-				TranslateMessage(&msg);
+				::TranslateMessage(&msg);
 				DispatchMessage(&msg);
 			}
 		}
@@ -104,14 +105,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch(uMsg)
 	{
 	case WM_DESTROY:
-		PostQuitMessage(0);
+		::PostQuitMessage(0);
 		break;
 
 	case WM_KEYDOWN:
 		switch(wParam)
 		{
 		case VK_ESCAPE:
-			DestroyWindow(hWnd);
+			::DestroyWindow(hWnd);
 			break;
 		}
 		break;
