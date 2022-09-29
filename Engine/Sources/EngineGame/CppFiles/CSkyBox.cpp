add9cf5f0490f41758f3de597d4cd12558988b91
MaGaNeT <yhcmaganet@gmail.com>
2022-09-29 19:31:05 +0800

    Add sky box

---
+++ Engine/Sources/EngineGame/CppFiles/CSkyBox.cpp
@@ -0,0 +1,31 @@
+#include "../../../../Entry/EngineMain.h"
+#include "../Headers/CSkyBox.h"
+#include "../../EngineRender/RenderBase/Headers/CRenderDevice.h"
+#include "../../EngineRender/AssetsManager/Headers/CShader.h"
+#include "../../EngineRender/AssetsManager/Headers/CShaderManager.h"
+#include "../../EngineRender/RenderBase/Headers/CMeshRenderer.h"
+
+CSkyBox::CSkyBox()
+{
+	this->m_SkyBoxInfo = SkyBoxInfo(static_cast<FLOAT>(1000000));
+}
+void CSkyBox::Init()
+{
+
+}
+void CSkyBox::Uninit()
+{
+
+}
+void CSkyBox::Update()
+{
+
+}
+void CSkyBox::PrepareDraw()
+{
+
+}
+void CSkyBox::Draw()
+{
+
+}
---
+++ Engine/Sources/EngineGame/Headers/CSkyBox.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class CSkyBox
+{
+public:
+	struct SkyBoxInfo
+	{
+		SkyBoxInfo() { ::ZeroMemory(this, sizeof(*this)); }
+		SkyBoxInfo(const FLOAT& radius)
+		{
+			Radius = radius;
+		}
+		FLOAT Radius;
+	};
+public:
+	virtual void	Init();
+	virtual void	Uninit();
+	virtual void	Update();
+	virtual void	Draw();
+protected:
+	virtual void	PrepareDraw();
+public:
+	CSkyBox();
+	virtual ~CSkyBox() {}
+protected:
+	SkyBoxInfo	m_SkyBoxInfo;
+protected:
+	std::shared_ptr<class CVertexShader>	m_VertexShader;
+	std::shared_ptr<class CPixelShader>		m_PixelShader;
+};
