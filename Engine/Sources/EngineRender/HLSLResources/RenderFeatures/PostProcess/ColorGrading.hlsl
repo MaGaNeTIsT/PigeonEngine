67dcad51f0354039ba732bec4875ec8d532c45b8
MaGaNeT <yhcmaganet@gmail.com>
2022-12-10 12:54:14 +0800

    Add new read assimp style

--- Engine/Sources/EngineRender/HLSLResources/RenderFeatures/PostProcess/ColorGrading.hlsl
+++ Engine/Sources/EngineRender/HLSLResources/RenderFeatures/PostProcess/ColorGrading.hlsl
@@ -7,7 +7,7 @@ float4 main(VaryingScreenPolygon2D input) : SV_Target
 {
 	float4 color = float4(0.0,0.0,0.0,1.0);
 	color.rgb = _EngineSceneColor.Sample(_LinearClampSampler, input.uv0).rgb;
-	color.rgb = pow(color.rgb, 0.41667);
+	color.rgb = pow(abs(color.rgb), 0.41667);
 	return color;
 }
 
