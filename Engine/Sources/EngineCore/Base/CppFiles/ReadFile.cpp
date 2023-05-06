e4234124e31acae64b102861e220ee411f254dc4
LorewalkerYu <547251722@qq.com>
2023-05-06 16:29:42 +0800

    FileRead

--- Engine/Sources/EngineCore/Base/CppFiles/ReadFile.cpp
+++ Engine/Sources/EngineCore/Base/CppFiles/ReadFile.cpp
@@ -1,1 +1,34 @@
-#include "../Headers/ReadFile.h"
+﻿#include "../Headers/ReadFile.h"
+#include <fstream>
+bool PigeonEngine::EReadFile::ReadFileAsBinary(const std::string FilePath, void*& Return, UINT64& Size)
+{
+	using namespace std;
+	std::ifstream  fin(FilePath, ios::in | ios::binary);;
+	if (!fin)
+	{
+		// cout << "Error Occured" << endl;
+		return false;
+	}
+	fin.seekg(0, ios_base::end);
+	Size = fin.tellg();
+	fin.seekg(0, ios_base::beg);
+	Return = new char[Size];
+	fin.read((char*)Return, sizeof(char) * Size);
+	fin.close();
+	return true;
+}
+
+bool PigeonEngine::EReadFile::ReadFileAsString(const std::string FilePath, std::string& Return)
+{
+	using namespace std;
+	ifstream fin(FilePath, ios::in);
+	if (!fin)
+	{
+		// cout << "Error Occured" << endl;
+		return false;
+	}
+	istreambuf_iterator<char> beg(fin), end;
+	Return = string(beg, end);
+	fin.close();
+	return true;
+}
--- Engine/Sources/EngineCore/Base/Headers/ReadFile.h
+++ Engine/Sources/EngineCore/Base/Headers/ReadFile.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "../../Core/Headers/Main.h"
 
@@ -15,6 +15,40 @@ namespace PigeonEngine
 		~EReadFile() {}
 	public:
 		EReadFile& operator=(const EReadFile&) = delete;
+
+	public:
+		static bool ReadFileAsBinary(const std::string FilePath, void*& Return, UINT64& Size);
+		static bool ReadFileAsString(const std::string FilePath, std::string& Return);
 	};
 
 };
+
+
+ /* Test Code ReadFileAsBinary */
+ //const std::string FilePath = "C:/Users/Administrator/Desktop/[ファミ通書籍編集部] 白夜極光　公式アート＆シナリオブック/OEBPS/Images/embed0000.jpg";
+ //void* File = nullptr;
+ //unsigned long FileSize = 0;
+ //if (ReadFileAsBinary(FilePath, File, FileSize))
+ //{
+ //	for (unsigned long i = 0; i < FileSize; ++i)
+ //	{
+ //		if (i % 16 != 0)
+ //		{
+ //			printf(" ");
+ //			printf("%02x", (unsigned char)((char*)File)[i]);
+ //		}
+ //		else
+ //		{
+ //			printf("\n");
+ //		}
+ //	}
+ //}
+
+ /* Test Code ReadFileAsString */
+//const std::string FilePath = "C:/Users/Administrator/Desktop/12312/readme.txt";
+//unsigned long FileSize = 0;
+//std::string File;
+//if (ReadFileAsString(FilePath, File))
+//{
+//	std::cout << File << std::endl;
+//}
