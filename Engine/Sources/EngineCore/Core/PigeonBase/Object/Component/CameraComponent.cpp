395a8f4a26c15845cd0d5ff49efcd97792834dba
MaGaNeT <yhcmaganet@gmail.com>
2023-05-18 10:56:59 +0800

    New file structure ver 0.6.6
    Add render core config and device folder and files

--- Engine/Sources/EngineCore/Core/PigeonBase/Object/Component/CameraComponent.cpp
+++ Engine/Sources/EngineCore/Core/PigeonBase/Object/Component/CameraComponent.cpp
@@ -9,5 +9,19 @@ namespace PigeonEngine
 	PCameraComponent::~PCameraComponent()
 	{
 	}
+	void PCameraComponent::SetViewport(FLOAT InTopLeftX, FLOAT InTopLeftY, FLOAT InWidth, FLOAT InHeight)
+	{
+		Viewport.TopLeftX = InTopLeftX;
+		Viewport.TopLeftY = InTopLeftY;
+		Viewport.Width = InWidth;
+		Viewport.Height = InHeight;
+	}
+	void PCameraComponent::SetViewport(const Vector4& InViewRect)
+	{
+		Viewport.TopLeftX = InViewRect.x;
+		Viewport.TopLeftY = InViewRect.y;
+		Viewport.Width = InViewRect.z;
+		Viewport.Height = InViewRect.w;
+	}
 
 };
--- Engine/Sources/EngineCore/Core/PigeonBase/Object/Component/CameraComponent.h
+++ Engine/Sources/EngineCore/Core/PigeonBase/Object/Component/CameraComponent.h
@@ -7,6 +7,46 @@
 namespace PigeonEngine
 {
 
+	struct PCameraViewInfo
+	{
+		PCameraViewInfo() : Viewport(RViewport(Vector2::Zero(), Vector2::One(), Vector2::YVector())) {}
+
+		RViewport	Viewport;
+		FLOAT		Fov;
+		FLOAT		Far;
+		FLOAT		Near;
+	};
+
+	struct PCameraFrustumInfo
+	{
+		union
+		{
+			struct
+			{
+				Vector3	PlaneTop;
+				Vector3	PlaneBottom;
+				Vector3	PlaneLeft;
+				Vector3	PlaneRight;
+			};
+			Vector3	Plane[4];
+		};
+		union
+		{
+			struct
+			{
+				Vector3	FarPlaneTopLeft;
+				Vector3	FarPlaneTopRight;
+				Vector3	FarPlaneBottomLeft;
+				Vector3	FarPlaneBottomRight;
+				Vector3	NearPlaneTopLeft;
+				Vector3	NearPlaneTopRight;
+				Vector3	NearPlaneBottomLeft;
+				Vector3	NearPlaneBottomRight;
+			};
+			Vector3	FarNearPlanePoint[8];
+		};
+	};
+
 	class PCameraComponent : public PSceneComponent
 	{
 
@@ -15,7 +55,16 @@ namespace PigeonEngine
 		CLASS_VIRTUAL_NOCOPY_BODY(PCameraComponent)
 
 	public:
-
+		const PCameraViewInfo&		GetCameraViewInfo()const { return CameraViewInfo; }
+		const PCameraFrustumInfo&	GetCameraFrustumInfo()const { return CameraFrustumInfo; }
+	public:
+		// View port is a rect that defines anchor(top left point), width(x axis length) and height(y axis length).
+		void SetViewport(FLOAT InTopLeftX, FLOAT InTopLeftY, FLOAT InWidth, FLOAT InHeight);
+		// Param [InViewRect] : x is top left x. y is top left y. z is width. w is height.
+		void SetViewport(const Vector4& InViewRect);
+	protected:
+		PCameraViewInfo		CameraViewInfo;
+		PCameraFrustumInfo	CameraFrustumInfo;
 	};
 
 };
