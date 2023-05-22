8d48c3314f0a9f82a006f2cc26971a0454bae55a
MaGaNeT <yhcmaganet@gmail.com>
2023-05-22 11:25:56 +0800

    Define header and project config

--- Engine/Sources/EngineCore/Main/Main.h
+++ Engine/Sources/EngineCore/Main/Main.h
@@ -1,5 +1,31 @@
 #pragma once
 
+#ifndef _DEBUG_MODE
+#define _DEBUG_MODE 0
+#else
+#define _DEBUG_MODE 1
+#endif
+#ifndef _DEVELOP_MODE
+#define _DEVELOP_MODE 0
+#else
+#define _DEVELOP_MODE 1
+#endif
+#ifndef _TEST_MODE
+#define _TEST_MODE 0
+#else
+#define _TEST_MODE 1
+#endif
+#ifndef _SHIPPING_MODE
+#define _SHIPPING_MODE 0
+#else
+#define _SHIPPING_MODE 1
+#endif
+#if (_DEBUG_MODE || _DEVELOP_MODE)
+#define _EDITOR_ONLY 1
+#else
+#define _EDITOR_ONLY 0
+#endif
+
 #include <SDKDDKVer.h>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
