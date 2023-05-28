36c00276a6a99b480de1519ef43fa022b6c67a39
paidax79 <60389207+paidax79@users.noreply.github.com>
2023-05-28 16:51:12 +0800

    Upload Jolt library

---
+++ Engine/EngineThirdParty/JoltPhysics/Headers/JoltPhysics.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Jolt/Jolt.h>
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+
---
+++ Engine/EngineThirdParty/JoltPhysics/Headers/PhysicsManager.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <CoreMinimal.h>
+#include "JoltPhysics.h"
+
+namespace PigeonEngine
+{
+};
