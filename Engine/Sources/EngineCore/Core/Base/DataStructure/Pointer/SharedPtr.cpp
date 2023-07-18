76bd5997656ca7588849183e145c6a07b3dd7ff0
LorewalkerYu <547251722@qq.com>
2023-07-18 16:53:23 +0800

    World v004
    Move implementation from .h to .cpp for some container classes

--- Engine/Sources/EngineCore/Core/Base/DataStructure/Pointer/SharedPtr.cpp
+++ Engine/Sources/EngineCore/Core/Base/DataStructure/Pointer/SharedPtr.cpp
@@ -1,1 +1,79 @@
 ﻿#include "SharedPtr.h"
+
+namespace PigeonEngine
+{
+    template <typename T>
+ TSharedPtr<T>::TSharedPtr()
+    {
+    }
+
+    template <typename T>
+    TSharedPtr<T>::TSharedPtr(const TSharedPtr<T>& Other)
+        :
+    Shared(Other.Shared)
+    {
+    }
+
+    template <typename T>
+    TSharedPtr<T>::TSharedPtr(const std::shared_ptr<T>& Other)
+        :
+    Shared(Other)
+    {
+    }
+
+    template <typename T>
+    T& TSharedPtr<T>::operator*() const
+    {
+        return *Shared;
+    }
+
+    template <typename T>
+    T* TSharedPtr<T>::operator->() const
+    {
+        return Shared.get();
+    }
+
+    template <typename T>
+    TSharedPtr<T>& TSharedPtr<T>::operator=(const TSharedPtr<T>& Other)
+    {
+        Shared = Other.Shared;
+        return *this;
+    }
+
+    template <typename T>
+    TSharedPtr<T>& TSharedPtr<T>::operator=(const std::shared_ptr<T>& Other)
+    {
+        Shared = Other;
+        return *this;
+    }
+
+    template <typename T>
+    TSharedPtr<T>::operator bool() const
+    {
+        return IsValid();
+    }
+
+    template <typename T>
+    BOOL TSharedPtr<T>::IsValid() const
+    {
+        return Shared.get() == nullptr;
+    }
+
+    template <typename T>
+    T* TSharedPtr<T>::Get() const
+    {
+        return Shared.get();
+    }
+
+    template <typename T>
+    void TSharedPtr<T>::Reset()
+    {
+        Shared.reset();
+    }
+
+    template <typename T>
+    TSharedPtr<T> TSharedPtr<T>::MakeShared()
+    {
+        return TSharedPtr<T>(std::make_shared<T>());
+    }
+}
