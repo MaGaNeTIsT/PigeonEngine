c6e667092150a4d4d6460aa9fcc22827d26d659b
LorewalkerYu <547251722@qq.com>
2023-07-18 16:53:45 +0800

    World V005
    Add OctTree Template class

---
+++ Engine/Sources/EngineCore/Core/Base/DataStructure/Container/OctTree.cpp
@@ -0,0 +1,117 @@
+﻿#include "OctTree.h"
+namespace PigeonEngine
+{
+    template <typename T>
+    TOctTreeNodeContent<T>::TOctTreeNodeContent()
+    {
+        
+    }
+
+    template <typename T>
+    TOctTreeNodeContent<T>::~TOctTreeNodeContent()
+    {
+        Content.Clear();
+    }
+
+    template <typename T>
+    TOctTreeNodeContent<T>::TOctTreeNodeContent(const TArray<TSharedPtr<T>>& InContent)
+        :
+    Content(InContent)
+    {
+        
+    }
+
+    template <typename T>
+    TArray<TSharedPtr<T>>& TOctTreeNodeContent<T>::GetContent()
+    {
+        return Content;
+    }
+
+    template <typename T>
+    void TOctTreeNodeContent<T>::SetContent(const TArray<TSharedPtr<T>>& InContent)
+    {
+        this->Content = InContent;
+    }
+
+    template <typename T>
+    void TOctTreeNodeContent<T>::AddItem(const TSharedPtr<T>& InItem)
+    {
+        Content.Add(InItem);
+    }
+
+    template <typename T>
+    void TOctTreeNodeContent<T>::RemoveItem(const TSharedPtr<T>& InItem)
+    {
+        Content.Remove(InItem);
+    }
+
+    template <typename T>
+    TOctTreeNode<T>::TOctTreeNode(const TSharedPtr<TOctTreeNode<T>>& InParent, const UINT& CurrentDepth,
+        const UINT& MaxDepth, const TArray<TSharedPtr<TOctTreeNode<T>>>& InContent)
+        :
+    Parent(InParent),
+    Content(InContent)
+    {
+        if(CurrentDepth < MaxDepth)
+        {
+            for(int i = 0; i < 8; ++i)
+            {
+                TSharedPtr<TOctTreeNode<T>> NewNode = TSharedPtr<T>::MakeShared(new TOctTreeNode<T>(this, CurrentDepth + 1, MaxDepth));
+            }
+        }
+    }
+
+    template <typename T>
+    BOOL TOctTreeNode<T>::IsLeafNode() const
+    {
+        return Children.Length() == 0;
+    }
+
+    template <typename T>
+    BOOL TOctTreeNode<T>::IsRootNode() const
+    {
+        return Parent.IsValid();
+    }
+
+    template <typename T>
+    TArray<TSharedPtr<T>>& TOctTreeNode<T>::GetContent()
+    {
+        return Content.GetContent();
+    }
+
+    template <typename T>
+    void TOctTreeNode<T>::SetContent(const TArray<TSharedPtr<T>>& InContent)
+    {
+        Content.SetContent(InContent);
+    }
+
+    template <typename T>
+    void TOctTreeNode<T>::AddItemToContent(const TSharedPtr<T>& InItem)
+    {
+        Content.AddItem(InItem);
+    }
+
+    template <typename T>
+    void TOctTreeNode<T>::RemoveItemInContent(const TSharedPtr<T>& InItem)
+    {
+        Content.RemoveItem(InItem);
+    }
+
+    template <typename T>
+    TOctTree<T>::TOctTree(const UINT& InDepth)
+        :
+    Depth(InDepth)
+    {
+        if(Depth < 1)
+        {
+            return;
+        }
+        Root = TSharedPtr<T>::MakeShared(new TOctTreeNode<T>(nullptr, 1, Depth));
+    }
+
+    template <typename T>
+    BOOL TOctTree<T>::IsValidTree() const
+    {
+        return Root.IsValid();
+    }
+}
---
+++ Engine/Sources/EngineCore/Core/Base/DataStructure/Container/OctTree.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include "Array.h"
+#include "Base/DataStructure/Pointer/SharedPtr.h"
+
+namespace PigeonEngine
+{
+    template <typename T>
+    class TOctTreeNodeContent
+    {
+    public:
+        TOctTreeNodeContent();
+        ~TOctTreeNodeContent();
+        explicit TOctTreeNodeContent(const TArray<TSharedPtr<T>>& InContent);
+        
+    public:
+        TArray<TSharedPtr<T>>& GetContent();
+        void                   SetContent(const TArray<TSharedPtr<T>>& InContent);
+        
+        void AddItem   (const TSharedPtr<T>& InItem);
+        void RemoveItem(const TSharedPtr<T>& InItem);
+    private:
+        TArray<TSharedPtr<T>> Content;
+    };
+
+    
+    template <typename T>
+    class TOctTreeNode
+    {
+    public:
+        TOctTreeNode() = delete;
+        TOctTreeNode(const TSharedPtr<TOctTreeNode<T>>& InParent, const UINT& CurrentDepth, const UINT& MaxDepth, const TArray<TSharedPtr<TOctTreeNode<T>>>& InContent = {});
+        
+    public:
+        ENGINE_NODISCARD BOOL IsLeafNode() const;
+        ENGINE_NODISCARD BOOL IsRootNode() const;
+
+    private:
+        TSharedPtr<TOctTreeNode<T>>         Parent = nullptr;
+        TArray<TSharedPtr<TOctTreeNode<T>>> Children;
+        
+    public:
+        TArray<TSharedPtr<T>>& GetContent();
+        void                   SetContent(const TArray<TSharedPtr<T>>& InContent);
+        
+        void AddItemToContent   (const TSharedPtr<T>& InItem);
+        void RemoveItemInContent(const TSharedPtr<T>& InItem);
+    private:
+        TOctTreeNodeContent<T>  Content;
+    };
+
+    
+    template <typename T>
+    class TOctTree
+    {
+    public:
+        TOctTree() = delete;
+        explicit TOctTree(const UINT& InDepth);
+
+    public:
+        ENGINE_NODISCARD BOOL IsValidTree() const;
+    private:
+        TSharedPtr<TOctTreeNode<T>> Root = nullptr;
+        UINT Depth = 0;
+    };
+
+}
