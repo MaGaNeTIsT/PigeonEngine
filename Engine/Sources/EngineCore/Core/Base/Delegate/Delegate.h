b81ac433f2af61fedd62c767435784e59a03a984
LorewalkerYu <547251722@qq.com>
2023-08-29 17:23:21 +0800

    Function and Delegate v001

---
+++ Engine/Sources/EngineCore/Core/Base/Delegate/Delegate.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "Function.h"
+#include "Base/DataStructure/Container/Array.h"
+
+
+namespace PigeonEngine
+{
+    template <typename FuncType>
+    class TDelegate;
+    template <typename Ret, typename... Args>
+    class TDelegate<Ret (Args...)>
+    {
+    public:
+        void Add(const TFunction<Ret (Args...)>& New);
+        void Excute(Args... Arguments);
+    private:
+        TArray<TFunction<Ret (Args...)>> ToCall;
+    };
+
+    template <typename Ret, typename ... Args>
+    void TDelegate<Ret(Args...)>::Add(const TFunction<Ret(Args...)>& New)
+    {
+        ToCall.Add(New);
+    }
+
+    template <typename Ret, typename ... Args>
+    void TDelegate<Ret(Args...)>::Excute(Args... Arguments)
+    {
+        for(const auto& Elem : ToCall)
+        {
+            Elem(Arguments...);
+        }
+    }
+}
---
+++ Engine/Sources/EngineCore/Core/Base/Delegate/Function.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <functional>
+
+namespace PigeonEngine
+{
+    // use std::function for now.
+    template <typename FuncType>
+    class TFunction;
+
+    template <typename Ret, typename... Args>
+    class TFunction<Ret (Args...)>
+    {
+    public:
+        TFunction(std::function<Ret(Args...)> Op) : Operation(Op) {}
+
+        Ret operator()(Args... Arguments){return Operation(Arguments...);}
+    private:
+        std::function<Ret(Args...)> Operation;
+    };
+
+    // // examples
+    // 1, for a normal function,
+    // int Test(int any)
+    // {
+    //     return any;
+    // }
+    // TFunction<int(int)> Func(Test);
+    // int out = Func(5);
+    // ---------------------------------
+    // 2, for a member function.
+    // class A
+    // {
+    // public:
+    //     int Test(int any)
+    //     {
+    //         return any;
+    //     }
+    // };
+    // A a;
+    // TFunction<int(int)> Func([&a](int m){return a.Test(m);});
+    // int out = Func(5);
+    // ---------------------------------
+}
