7b154885ce15f99b4644aae2078e2d61ea1aa133
LorewalkerYu <547251722@qq.com>
2023-09-23 17:34:42 +0800

    Update FileHelper.cpp

--- Engine/Sources/EngineCore/IO/FileHelper.cpp
+++ Engine/Sources/EngineCore/IO/FileHelper.cpp
@@ -8,8 +8,12 @@ namespace PigeonEngine
 	BOOL8 EFileHelper::ReadFileAsBinary(const EString& FilePath, void*& Return, ULONG& Size)
 	{
 		std::ifstream file(*FilePath, std::ios::binary);
-		PE_CHECK(ENGINE_FILE_ERROR, "Load file failed", file.is_open())
-
+		// PE_CHECK(ENGINE_FILE_ERROR, "EFileHelper::ReadFileAsBinary : Load file failed", file.is_open())
+		if(!file.is_open())
+		{
+			file.close();
+			return FALSE;
+		}
 		// 获取文件长度
 		file.seekg(0, std::ios::end);
 		Size = static_cast<ULONG>(file.tellg());
@@ -26,8 +30,11 @@ namespace PigeonEngine
 	BOOL8 EFileHelper::ReadFileAsString(const EString& FilePath, EString& Return)
 	{
 		std::ifstream file(*FilePath);
-		PE_CHECK(ENGINE_FILE_ERROR, "Load file failed", file.is_open())
-
+		if(!file.is_open())
+		{
+			file.close();
+			return FALSE;
+		}
 		// 使用流操作符将文件内容读取到字符串中
 		std::string line;
 		Return = "";
@@ -45,7 +52,7 @@ namespace PigeonEngine
 		{
 			if(bCreateDirectory)
 			{
-				PE_CHECK(ENGINE_FILE_ERROR, "Create directory failed when save string to file", std::filesystem::create_directories(directoryPath))
+				PE_CHECK(ENGINE_FILE_ERROR, "EFileHelper::SaveStringToFile : Create directory failed when save string to file", std::filesystem::create_directories(directoryPath))
 			}
 			else
 			{
@@ -53,7 +60,7 @@ namespace PigeonEngine
 			}
 		}
 		std::ofstream out(*FilePath);
-		PE_CHECK(ENGINE_FILE_ERROR, "save string to file failed", out.is_open())
+		PE_CHECK(ENGINE_FILE_ERROR, "EFileHelper::SaveStringToFile : save string to file failed", out.is_open())
 		out << (*Str);
 		out.close();
 		return TRUE;
@@ -66,7 +73,7 @@ namespace PigeonEngine
 		{
 			if(bCreateDirectory)
 			{
-				PE_CHECK(ENGINE_FILE_ERROR, "Create directory failed when save string to file", std::filesystem::create_directories(directoryPath))
+				PE_CHECK(ENGINE_FILE_ERROR, "EFileHelper::SaveBytesToFile : Create directory failed when save string to file", std::filesystem::create_directories(directoryPath))
 			}
 			else
 			{
@@ -75,7 +82,7 @@ namespace PigeonEngine
 		}
 		
 		std::ofstream out(*FilePath, std::ios::binary);
-		PE_CHECK(ENGINE_FILE_ERROR, "Save bytes to file failed", out.is_open())
+		PE_CHECK(ENGINE_FILE_ERROR, "EFileHelper::SaveBytesToFile : Save bytes to file failed", out.is_open())
 		out.write(static_cast<const CHAR*>(Bytes), Size);
 		out.close();
 		return TRUE;
