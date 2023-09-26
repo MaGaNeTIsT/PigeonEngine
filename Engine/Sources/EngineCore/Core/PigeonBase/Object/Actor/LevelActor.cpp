6f46f1cc82340c1d128ffb066ef53163386b3c93
LorewalkerYu <547251722@qq.com>
2023-09-26 20:04:17 +0800

    add a directional light

--- Engine/Sources/EngineCore/Core/PigeonBase/Object/Actor/LevelActor.cpp
+++ Engine/Sources/EngineCore/Core/PigeonBase/Object/Actor/LevelActor.cpp
@@ -5,6 +5,7 @@
 #include "MeshAsset/MeshAsset.h"
 #include "../Controller/Controller.h"
 #include "Base/Timer/Timer.h"
+#include "PigeonBase/Object/Component/CameraAndLight/DirectionalLightComponent.h"
 
 namespace PigeonEngine
 {
@@ -31,23 +32,35 @@ namespace PigeonEngine
 
 	void PLevelActor::UserBeginPlay()
     {
-		PActor* New = new PActor();
-    	New->SetIsTickable(TRUE);
-		this->GetWorld()->AddActor(New);
+    	// add a static mesh actor
+    	{
+    		PActor* New = new PActor();
+    		New->SetIsTickable(TRUE);
+    		this->GetWorld()->AddActor(New);
     	
-		PStaticMeshComponent* NewStaticMeshComp = new PStaticMeshComponent();
-    	NewStaticMeshComp->SetIsTickable(TRUE);
-		New->AddComponent(NewStaticMeshComp);
+    		PStaticMeshComponent* NewStaticMeshComp = new PStaticMeshComponent();
+    		NewStaticMeshComp->SetIsTickable(TRUE);
+    		New->AddComponent(NewStaticMeshComp);
 
-		const EStaticMeshAsset* Asset = nullptr;
-		EString ImportPath("./Engine/Assets/EngineModels/SceneModels/Camera/");
-		EString ImportName("Camera");
-		EString ImportFileType("obj");
-		TryLoadStaticMesh(ESettings::ENGINE_MESH_PATH, "CameraMesh", Asset, &ImportPath, &ImportName, &ImportFileType);
+    		const EStaticMeshAsset* Asset = nullptr;
+    		EString ImportPath("./Engine/Assets/EngineModels/SceneModels/Camera/");
+    		EString ImportName("Camera");
+    		EString ImportFileType("obj");
+    		TryLoadStaticMesh(ESettings::ENGINE_MESH_PATH, "CameraMesh", Asset, &ImportPath, &ImportName, &ImportFileType);
 
 
-		NewStaticMeshComp->SetMeshAsset(Asset);
-		
+    		NewStaticMeshComp->SetMeshAsset(Asset);
+    	}
+		// 
+    	{
+    		PActor* New = new PActor();
+    		New->SetIsTickable(TRUE);
+    		this->GetWorld()->AddActor(New);
+			PDirectionalLightComponent* LightComp = new PDirectionalLightComponent();
+    		New->AddComponent(LightComp);
+
+    		
+    	}
 		this->GetWorld()->GetController()->SetActorLocation(Vector3(0, 0, -100));
     }
 
