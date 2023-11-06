6fdaf17d3085c46fe69f362e477ae31e6fe432da
MaGaNeT <yhcmaganet@gmail.com>
2023-11-06 17:27:46 +0800

    Material Ver0.1

--- Engine/Sources/EngineCore/Core/PigeonBase/Object/Material.h
+++ Engine/Sources/EngineCore/Core/PigeonBase/Object/Material.h
@@ -2,7 +2,6 @@
 
 #include <CoreMinimal.h>
 #include "Object.h"
-#include <MaterialAsset/MaterialAsset.h>
 #include <ShaderAsset/ShaderAsset.h>
 #include <Shader/ShaderParameter.h>
 
@@ -11,6 +10,31 @@ namespace PigeonEngine
 
 	class PMaterial : public PObject
 	{
+	public:
+		enum EMaterialParameterType : UINT32
+		{
+			MATERIAL_PARAMETER_TYPE_FLOAT1,
+			MATERIAL_PARAMETER_TYPE_FLOAT2,
+			MATERIAL_PARAMETER_TYPE_FLOAT3,
+			MATERIAL_PARAMETER_TYPE_FLOAT4,
+			MATERIAL_PARAMETER_TYPE_INT1,
+			MATERIAL_PARAMETER_TYPE_INT2,
+			MATERIAL_PARAMETER_TYPE_INT3,
+			MATERIAL_PARAMETER_TYPE_INT4,
+			MATERIAL_PARAMETER_TYPE_UINT1,
+			MATERIAL_PARAMETER_TYPE_UINT2,
+			MATERIAL_PARAMETER_TYPE_UINT3,
+			MATERIAL_PARAMETER_TYPE_UINT4,
+			MATERIAL_PARAMETER_TYPE_MATRIX33,
+			MATERIAL_PARAMETER_TYPE_MATRIX34,
+			MATERIAL_PARAMETER_TYPE_MATRIX44,
+			MATERIAL_PARAMETER_TYPE_TEXTURE2D,
+			MATERIAL_PARAMETER_TYPE_TEXTURECUBE,
+			MATERIAL_PARAMETER_TYPE_TEXTURE3D,
+			MATERIAL_PARAMETER_TYPE_COUNT
+		};
+	public:
+		void	AddParameter(EMaterialParameterType InType, const EString& InParamName = "_Param");
 	protected:
 		EMaterialParameter			MaterialParameter;
 		const EVertexShaderAsset*	VertexShader;
