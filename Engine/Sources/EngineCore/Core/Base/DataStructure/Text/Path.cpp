f1aa81d9679937f08b77279fc66a320b2ab706b8
paidax79 <60389207+paidax79@users.noreply.github.com>
2023-11-30 01:20:53 +0800

    upload Path

---
+++ Engine/Sources/EngineCore/Core/Base/DataStructure/Text/Path.cpp
@@ -0,0 +1,128 @@
+﻿#include "Path.h"
+#include <Config/ErrorCaption.h>
+#include <Base/Math/Math.h>
+#include <Config/EngineConfig.h>
+#include "../../../../../Development/Alert/DevelopmentDefines.h"
+#include <filesystem>
+
+namespace PigeonEngine
+{
+	UINT32 FindFirstSlashIndex(const EString& Path)
+	{
+		const CHAR slash[] = { '/' ,'\0' };
+		UINT32 FirstSlash = Path.Find(slash);
+		if (Path.Length() >= (FirstSlash + 1u))
+		{
+			const CHAR backslash[] = { '\\', '\0' };
+			FirstSlash = Path.Find(backslash);
+			PE_CHECK((ENGINE_STRING_ERROR), ("Check string rest length error."), (Path.Length() >= (FirstSlash + 1u)));
+		}
+		return FirstSlash;
+	};
+
+	UINT32 FindLastSlashIndex(const EString& Path)
+	{
+		const CHAR slash[] = { '/' ,'\0' };
+		UINT32 LastSlash = Path.RightFind(slash);
+		if (Path.Length() >= (LastSlash + 1u))
+		{
+			const CHAR backslash[] = { '\\', '\0' };
+			LastSlash = Path.RightFind(backslash);
+			PE_CHECK((ENGINE_STRING_ERROR), ("Check string rest length error."), (Path.Length() >= (LastSlash + 1u)));
+		}
+		return LastSlash;
+	};
+
+	EString EPath::ChangeExtension(const EString& Path, const EString& Extension)
+	{
+		const CHAR Dot[] = { '.' ,'\0' };
+		const UINT32 LastDot = Path.RightFind(Dot);
+		PE_CHECK((ENGINE_STRING_ERROR), ("Check string rest length error."), (Path.Length() >= (LastDot + 1u)));
+		return Path.Substring(0u, LastDot + 1u) + Extension;
+	}
+
+	EString EPath::Combine(const EString& Path1, const EString& Path2)
+	{
+		return Path1 + "/" + Path2;
+	}
+
+	EString EPath::Combine(const EString& Path1, const EString& Path2, const EString& Path3)
+	{
+		return Path1 + "/" + Path2 + "/" + Path3;
+	}
+
+	EString EPath::Combine(const EString* Paths, INT32 Count)
+	{
+		EString result;
+		for (int i = 0; i < Count - 1; i++)
+		{
+			result += Paths[i] + "/";
+		}
+		result += Paths[Count - 1];
+		return result;
+	}
+	BOOL32 EPath::Exists(const EString& Path)
+	{
+		return std::filesystem::exists(Path.GetDataAsCopy());
+	}
+	EString EPath::GetDirectoryName(const EString& Path)
+	{
+		UINT32 LastSlash = FindLastSlashIndex(Path);
+		EString TempPath = Path.Substring(0u, LastSlash - 1u);
+		UINT32 LeastSlash = FindLastSlashIndex(TempPath);
+		return Path.Substring(LeastSlash + 1u, (LastSlash - LeastSlash) - 1u);
+	}
+	EString EPath::GetExtension(const EString& Path)
+	{
+		const CHAR Dot[] = { '.' ,'\0' };
+		const UINT32 LastDot = Path.RightFind(Dot);
+		PE_CHECK((ENGINE_STRING_ERROR), ("Check string rest length error."), (Path.Length() >= (LastDot + 1u)));
+		return Path.Substring(LastDot + 1u, Path.Length() - (LastDot + 1u));
+	}
+
+	EString EPath::GetFileNameWithExtension(const EString& Path)
+	{
+		UINT32 LastSlash = FindLastSlashIndex(Path);
+		return Path.Substring(LastSlash + 1u, Path.Length() - LastSlash + 1u);
+	}
+
+	EString EPath::GetFileNameWithoutExtension(const EString& Path)
+	{
+		UINT32 LastSlash = FindLastSlashIndex(Path);
+		const CHAR Dot[] = { '.' ,'\0' };
+		EString SubString = Path.Substring(LastSlash + 1u, Path.Length() - LastSlash + 1u);
+		const UINT32 LastDot = SubString.RightFind(Dot);
+		return SubString.Substring(0u, LastDot);
+	}
+
+	EString EPath::GetFullPath(const EString& Path)
+	{
+		return EString(EBaseSettings::ENGINE_ASSET_DIRECTORY) + Path;
+	}
+
+	EString EPath::GetFullPath(const EString& Path, const EString& BasePath)
+	{
+		return BasePath + "/" + Path;
+	}
+
+	EString EPath::GetRootPath(const EString& Path)
+	{
+		UINT32 SlashIndex = FindFirstSlashIndex(Path);
+		return Path.Substring(0, SlashIndex);
+	}
+
+	EString EPath::GetRelativePath(const EString& Path, const EString& BasePath)
+	{
+		if (Path.Contains(BasePath))
+		{
+			EString subString = Path.Replace(BasePath, "");
+			return subString.Substring(1, subString.LastIndex());
+		}
+		return EString();
+	}
+
+	BOOL32 EPath::HasExtension(const EString& Path)
+	{
+		return EPath::GetExtension(Path).Length() > 0;
+	}
+};
---
+++ Engine/Sources/EngineCore/Core/Base/DataStructure/Text/Path.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+#include "../../../../Main/Main.h"
+#include <Base/DataStructure/BuiltInType.h>
+#include <Base/DataStructure/BaseType.h>
+#include "String.h"
+
+
+namespace PigeonEngine
+{
+    class EPath
+    {
+    public:
+        /// <summary>
+        /// change extension of path
+        /// </summary>
+        /// <param name="Path">:path need change extension.</param>
+        /// <param name="Extension">:extension to change.</param>
+        static EString ChangeExtension(const EString& Path, const EString& Extension);
+        /// <summary>
+        /// Combine two path to one.
+        /// </summary>
+        static EString Combine(const EString& Path1,const EString& Path2);
+        /// <summary>
+        /// Combine three paths to one.
+        /// </summary>
+        static EString Combine(const EString& Path1, const EString& Path2, const EString& Path3);
+        /// <summary>
+        /// Combine a array of paths to one.
+        /// </summary>
+        static EString Combine(const EString* Paths,INT32 Count);
+        /// <summary>
+        /// true if path is exists in harddisk.
+        /// </summary>
+        static BOOL32 Exists(const EString& Path);
+        /// <summary>
+        /// get directory name of path.
+        /// </summary>
+        static EString GetDirectoryName(const EString& Path);
+        /// <summary>
+        /// get extension of path.
+        /// </summary>
+        static EString GetExtension(const EString& Path);
+        /// <summary>
+        /// get file name with extension.
+        /// </summary>
+        static EString GetFileNameWithExtension(const EString& Path);
+        /// <summary>
+        /// get file name without extension.
+        /// </summary>
+        static EString GetFileNameWithoutExtension(const EString& Path);
+        /// <summary>
+        /// get absolute path,
+        /// </summary>
+        /// <param name="Path">:relative path of asset path</param>
+        static EString GetFullPath(const EString& Path);
+        /// <summary>
+        /// get absolute path,
+        /// </summary>
+        /// <param name="Path">:relative path of base path</param>
+        static EString GetFullPath(const EString& Path, const EString& BasePath);
+        /// <summary>
+        /// get a string before first '/'.
+        /// </summary>
+        static EString GetRootPath(const EString& Path);
+        /// <summary>
+        /// get relative path,
+        /// </summary>
+        /// <param name="Path">:absolute path</param>
+        static EString GetRelativePath(const EString& Path, const EString& BasePath);
+        /// <summary>
+        /// check if path has extension.
+        /// </summary>
+        static BOOL32 HasExtension(const EString& Path);
+    };
+
+    extern UINT32 FindFirstSlashIndex(const EString& Path);
+    extern UINT32 FindLastSlashIndex(const EString& Path);
+};
+
